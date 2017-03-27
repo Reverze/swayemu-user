52d3ee05b0b9bce494364d97948ac3b169b7fa60
Reverze <hawkmedia24@gmail.com>
2017-03-27 21:28:14 +0200

    Add files via upload

--- src/SWUserStatement.php
+++ src/SWUserStatement.php
@@ -25,7 +25,7 @@ class SWUserStatement
      */
     public static function NickInUsing($nick)
     {
-        return \SWUserCore::getUserGeneric()->isUserNameAvailable($nick);
+        return !\SWUserCore::getUserGeneric()->isUserNameAvailable($nick);
     }
 
     /**
@@ -35,7 +35,7 @@ class SWUserStatement
      */
     public static function EmailInUsing($email)
     {
-        return \SWUserCore::getUserGeneric()->isEmailAddressAvailable($email);
+        return !\SWUserCore::getUserGeneric()->isEmailAddressAvailable($email);
     }
 
     public static function KeylogInUsing($keylog) : bool
